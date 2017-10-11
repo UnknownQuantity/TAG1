2e6beaababebbafbaeed8adcd01111f53f0eae6d
Joklin <Joklin@10.50.136.210>
2017-10-11 16:05:16 +0200

    dd

--- src/tag1/GameCtrl.java
+++ src/tag1/GameCtrl.java
@@ -3,4 +3,20 @@ package tag1;
 
 public class GameCtrl {
     
+    public void GameCtrl() {
+        
+        Player p1 = new Player();
+        Rooms r1 = new Rooms();
+        p1.Player();
+        r1.Rooms();
+        //r1.room.get(0); flyt ArrayList room ud af metoden (op under boolean roomInteraction)
+        r1.description(); //r1.description("Start"); muliggør dette
+        p1.Movement();
+        
+        
+        
+        
+        
+    }
+    
 }
