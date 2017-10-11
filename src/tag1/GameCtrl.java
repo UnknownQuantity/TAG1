3bb3965da832a1f380e43924253c781984eb89b0
Joklin <Joklin@10.50.136.210>
2017-10-11 16:39:33 +0200

    nyt til Jakob (kig GameCtrl)

--- src/tag1/GameCtrl.java
+++ src/tag1/GameCtrl.java
@@ -15,8 +15,15 @@ public class GameCtrl {
         
         
         
+        /*noter til Jakob:
+        jeg tænker at vi i Rooms skal tilføje et par booleans mere, en til loot og en til fare, og så skal rummet kalde
+        de to ting hvis de bliver triggered (f.eks hvis der er loot skal man kunne søge rummet) hvis faren så er loot
+        skal den naturligvis først kaldes hvis spilleren looter (f.eks den rådne mad)
+        Da det er rooms er det dig der skal implementere det for at vi ikke ødelægger det hele
         
-        
+        derudover har jeg lavet en movement i player men jeg er usikker på om den er kommet med, send gerne lige info via
+        facebook om det er i den version du har fået.
+        */
     }
     
 }
