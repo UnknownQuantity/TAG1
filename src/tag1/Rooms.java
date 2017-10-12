6cd024347aef7b3fb1489c0b36eeebdbaa1d3384
Dorte <Dorte@10.50.130.66>
2017-10-12 10:57:08 +0200

    Ændringer 11:00

--- src/tag1/Rooms.java
+++ src/tag1/Rooms.java
@@ -6,35 +6,38 @@ public class Rooms {
 
     boolean roomVisited = false; //Tracks wether or not you have been in a room (boolean true false)
     boolean roomInteraction = false; //Tracks the interactions in the room, if interaction is onetime use, set boolean true
-
+    ArrayList<RoomList> room = new ArrayList<>();
     ArrayList rooms = new ArrayList();
-
+    
+    
+    
+    
+    
     public void Rooms() {
-        ArrayList<RoomList> room = new ArrayList<>();
-        //north, east, south, west
-        room.add(new RoomList("Start", true, false, false, false));
-        room.add(new RoomList("Passage", true, true, true, true));
-        room.add(new RoomList("Death1", false, false, false, false));
-        room.add(new RoomList("Storage1", true, false, true, false));
-        room.add(new RoomList("Passage3", true, true, true, true));
-        room.add(new RoomList("Bedroom", false, true, false, true));
-        room.add(new RoomList("Loo2", false, true, false, true));
-        room.add(new RoomList("DeadEnd", false, false, true, false));
-        room.add(new RoomList("Corridor", false, true, false, true));
-        room.add(new RoomList("Passage2", true, true, true, true));
-        room.add(new RoomList("Loo", true, false, false, false));
-        room.add(new RoomList("Labratory", false, false, true, false));
-        room.add(new RoomList("Kitchen", true, true, false, true));
-        room.add(new RoomList("Pantry", true, true, false, false));
-        room.add(new RoomList("HiddenCorridor", true, false, true, false));
-        room.add(new RoomList("RoomOfRiches", false, false, true, false));
-        room.add(new RoomList("DiningHall", true, false, true, false));
-        room.add(new RoomList("Corridor2", false, true, true, false));
-        room.add(new RoomList("CourtYard", true, true, false, true));
-        room.add(new RoomList("Hall", false, true, true, true));
-        room.add(new RoomList("GuardRoom", true, false, false, false));
-        room.add(new RoomList("Freiheit", false, false, false, false));
-        room.add(new RoomList("Freiheit2", false, false, false, false));
+        //north, east, south, west, loot, danger
+        room.add(new RoomList("Start", true, false, false, false, false, false));
+        room.add(new RoomList("Passage", true, true, true, true, false, false));
+        room.add(new RoomList("Death1", false, false, false, false, false, true));
+        room.add(new RoomList("Storage1", true, false, true, false, true, false));
+        room.add(new RoomList("Passage3", true, true, true, true, false, false));
+        room.add(new RoomList("Bedroom", false, true, false, true, true, false));
+        room.add(new RoomList("Loo2", false, true, false, true, false, true));
+        room.add(new RoomList("DeadEnd", false, false, true, false, false, false));
+        room.add(new RoomList("Corridor", false, true, false, true, false, false));
+        room.add(new RoomList("Passage2", true, true, true, true, false, false));
+        room.add(new RoomList("Loo", true, false, false, false, true, false));
+        room.add(new RoomList("Labratory", false, false, true, false, true, true));
+        room.add(new RoomList("Kitchen", true, true, false, true, true, true));
+        room.add(new RoomList("Pantry", true, true, false, false, false, false));
+        room.add(new RoomList("HiddenCorridor", true, false, true, false, false, true)); //mathrandom 1 ud af 10 for at tage skade
+        room.add(new RoomList("RoomOfRiches", false, false, true, false, true, false));
+        room.add(new RoomList("DiningHall", true, false, true, false, false, true)); //If search room, danger activate
+        room.add(new RoomList("Corridor2", false, true, true, false, false, false));
+        room.add(new RoomList("CourtYard", true, true, false, true, false, false));
+        room.add(new RoomList("Hall", false, true, true, true, false, true)); // mathrandom i to 50 for a guard to walk out and beat ya
+        room.add(new RoomList("GuardRoom", true, false, false, false, false, true));
+        room.add(new RoomList("Freiheit", false, false, false, false, false, false));
+        room.add(new RoomList("Freiheit2", false, false, false, false, false, false));
 
     }
 
