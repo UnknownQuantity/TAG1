edbddf64e3c7cec454f9ab4eb6cd007bff89f679
Joklin <Joklin@10.50.136.214>
2017-10-25 13:42:38 +0200

    13:42 - update

--- src/tag1/Enemy.java
+++ src/tag1/Enemy.java
@@ -1,44 +1,71 @@
 package tag1;
 //WIP
-public class Enemy extends TAG1{
-    
-    private int health; 
+
+import java.util.Random;
+
+public class Enemy extends TAG1 {
+
+    private int health;
     private boolean hound = false;
-    
-public void monster() {
-    System.out.println("You meet a giant monster and is instantly killed");
-    health = game.player.getHealth();
-    game.player.setHealth(health = 0);
-} 
+    private long lastTurn = System.currentTimeMillis();
+    private int ran;
+    private String westMove, eastMove, northMove, southMove;
 
-public void movement() {
-    
-}
+    public void monster() {
+        System.out.println("You meet a giant monster and is instantly killed");
+        health = game.player.getHealth();
+        game.player.setHealth(health = 0);
+        ran = (int) Math.random() * 4 + 1;
+        switch (ran) {
+            case 1:
+                eastMove = "east";
+                break;
+            case 2:
+                westMove = "west";
+                break;
+            case 3:
+                northMove = "north";
+            case 4:
+                southMove = "south";
+                break;
+            default:
+                break;
+        }
+    }
 
-public void monster_Start() {
-    
-}
+    private Object Random() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
-public void hound() {
-    
-    if (!hound) {
-                int ran = (int) (3 * Math.random()) + 1;
-                System.out.println(ran);
-                if (ran == 3) {
+    public void movement() {
+
+    }
+
+    public void monster_Start() {
+
+    }
+
+    public void hound() {
+
+        if (!hound) {
+            int ran = (int) (3 * Math.random()) + 1;
+            System.out.println(ran);
+            if (ran == 3) {
                 System.out.println("The hound wakes up and jumps to attack you, it's teeth sinks into your arm and you lose 15 health");
                 System.out.println("The hound loses interest and leaves you alone");
                 health = game.player.getHealth();
-                game.player.setHealth(health -=15);
+                game.player.setHealth(health -= 15);
                 System.out.println("Your current health is now " + health);
                 game.setAreIn(true);
-                }
-                hound = true;
-            } else if (hound) {
-                System.out.println("The hound sees you immediately and once again jump to attack, you lose 15 health");
-                System.out.println("The hound loses interest and leaves you alone");
-                health = game.player.getHealth();
-                game.player.setHealth(health -=15);
-                System.out.println("Your current health is now " + health);
             }
-}
+            hound = true;
+        } else if (hound) {
+            System.out.println("The hound sees you immediately and once again jump to attack, you lose 15 health");
+            System.out.println("The hound loses interest and leaves you alone");
+            health = game.player.getHealth();
+            game.player.setHealth(health -= 15);
+            System.out.println("Your current health is now " + health);
+        }
+
+    }
 }
--- src/tag1/Items.java
+++ src/tag1/Items.java
@@ -1,134 +1,156 @@
 package tag1;
 
 public class Items extends TAG1 implements Item {
-    
+
     private String choice;
     private int health;
     private int gold;
     private int armor;
     private int damage;
-    
+
     public void minor_Potion() {
-        pickUpItem("a Minor Potion");
+        pickUpItem("a Minor Potion", '+');
     }
-    
+
     public void greater_Potion() {
-        pickUpItem("a Greater Potion");
+        pickUpItem("a Greater Potion", '+');
     }
-    
-    public void coin() {        
-        pickUpGold("a Coin");        
+
+    public void coin() {
+        pickUpGold("a Coin", '+');
         gold = game.player.getGold();
         game.player.setGold(gold += 1);
     }
-    
-    public void coins() {        
-        pickUpGold("Five Coins");        
+
+    public void coins() {
+        pickUpGold("Five Coins", '+');
         gold = game.player.getGold();
         game.player.setGold(gold += 5);
     }
-    
-    public void treasure() {        
-        pickUpGold("a Treasure");
+
+    public void treasure() {
+        pickUpGold("a Treasure", '+');
         System.out.println("The treasure holds 100 coins");
         gold = game.player.getGold();
         game.player.setGold(gold += 100);
     }
-    
+
     public void cloth_Armor() {
-        pickUpItem("Cloth Armor");
+        pickUpItem("Cloth Armor", '+');
         //armor + 10
     }
-    
+
     public void leather_Armor() {
-        pickUpItem("leather Armor");
+        pickUpItem("leather Armor", '+');
         //armor + 25
     }
-    
+
     public void plate_Armor() {
-        pickUpItem("Plate Armor");
+        pickUpItem("Plate Armor", '+');
         //armor 0+50
     }
-    
+
     public void sword() {
-        pickUpItem("Sword");
+        pickUpItem("Sword", '+');
         //damage + 15
     }
-    
+
     public void club() {
-        pickUpItem("Club");
+        pickUpItem("Club", '+');
         //damage + 10
     }
-    
+
     public void axe() {
-        pickUpItem("Axe");
+        pickUpItem("Axe", '+');
         //damage + 20
     }
-    
+
     public void fire_Bomb() {
-        pickUpItem("fire bomb");
+        pickUpItem("fire bomb", '+');
         //damage +30
     }
-    
+
     public void magical_Orb() {
-        pickUpItem("magical orb");
+        pickUpItem("magical orb", '+');
         //damage +50
     }
-    
+
     public void torch() {
-        pickUpItem("torch");
+        pickUpItem("torch", '+');
         //damage +15
     }
-    
+
     public void small_Bag() {
-        pickUpItem("small bag");
+        pickUpItem("small bag", '+');
         game.player.inventory_Expand("small bag");
     }
-    
+
     public void large_bag() {
-        pickUpItem("large bag");
+        pickUpItem("large bag", '+');
         game.player.inventory_Expand("large bag");
     }
-    
+
     @Override
     public void pickUpGold(String item, char es) {
-        System.out.println("You've found: " + item);
-        System.out.println("Do you want to pick it up?");
-        choice = game.scan.next();
-        if (choice.equals('y')) {
-            System.out.println("You've picked up " + item);
+        if (es == '+') {
+            System.out.println("You've found: " + item);
         }
-    }
-    
-    @Override
-    public void pickUpItem(String item, char es) {
-        System.out.println("You've found: " + item);
-        System.out.println("Do you want to pick it up?");
-        choice = game.scan.next();
-        if (choice.equals('y')) {
-            System.out.println("You've picked up " + item);
-            game.player.inventoryAdd(item);
-        }
-        else if (choice.equals('n'))
-            System.out.println("You leave the " + item + " on the groud");
-        else
-            System.out.println("invalid choice!");
-            pickUpItem(item, '-');
-    }
-    
-    @Override
-    public void pickUpPotion(String item, char es) {
-        System.out.println("You've found: " + item);
-        System.out.println("Do you want to pick it up?");
-        choice = game.scan.next();
-        if (choice.equals('y')) {
-            System.out.println("You've picked up " + item);
-            health = game.player.getHealth();
-            if (item.equals("a Minor Potion")) {
-                game.player.setHealth(health += 25);
-            } else if (item.equals("a Greater Potion")) {
-                game.player.setHealth(health += 50);
+        if (es == '+' || es == '-') {
+            System.out.println("Do you want to pick it up?");
+            choice = game.scan.next();
+            if (choice.equals('y')) {
+                System.out.println("You've picked up " + item);
+            } else if (choice.equals('n')) {
+                System.out.println("You leave the " + item + " on the groud");
+            } else {
+                System.out.println("Invalid choice!");
+                pickUpItem(item, '-');
             }
         }
-    }    
+    }
+
+    @Override
+    public void pickUpItem(String item, char es) {
+        if (es == '+') {
+            System.out.println("You've found: " + item);
+        }
+        if (es == '+' || es == '-') {
+            System.out.println("Do you want to pick up the item?");
+            choice = game.scan.next();
+            if (choice.equals('y')) {
+                System.out.println("You've picked up " + item);
+                game.player.inventoryAdd(item);
+            } else if (choice.equals('n')) {
+                System.out.println("You leave the " + item + " on the groud");
+            } else {
+                System.out.println("Invalid choice!");
+                pickUpItem(item, '-');
+            }
+        }
+    }
+
+    @Override
+    public void pickUpPotion(String item, char es) {
+        if (es == '+') {
+            System.out.println("You've found: " + item);
+        }
+        if (es == '+' || es == '-') {
+            System.out.println("Do you want to pick it up?");
+            choice = game.scan.next();
+            if (choice.equals('y')) {
+                System.out.println("You've picked up " + item);
+                health = game.player.getHealth();
+                if (item.equals("a Minor Potion")) {
+                    game.player.setHealth(health += 25);
+                } else if (item.equals("a Greater Potion")) {
+                    game.player.setHealth(health += 50);
+                }
+            } else if (choice.equals('n')) {
+                System.out.println("You leave the " + item + " on the gound");
+            } else {
+                System.out.println("Invalid choice!");
+                pickUpItem(item, '-');
+            }
+        }
+    }
 }
